47b1ec9e47ba40061c58c45dc18ca37dd3d98c6e
musti <musti@yahyas-mbp>
2020-01-07 17:30:39 +0100

    what

--- src/test2/Test.java
+++ src/test2/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hej");
+		System.out.print("Testing 1 2 3 . . . ");
+
 	}
 
 }
