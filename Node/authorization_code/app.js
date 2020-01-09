5f959a4b9c2775542875f3ad6f4b7d05b7c79dee
Patrik Nilsson <patriknilson@hotmail.com>
2020-01-09 20:09:15 +0100

    Testing, nothing done probably

--- Node/authorization_code/app.js
+++ Node/authorization_code/app.js
@@ -100,7 +100,8 @@ app.get('/callback', function(req, res) {
 
         // use the access token to access the Spotify Web API
         request.get(options, function(error, response, body) {
-          console.log(body);
+          //console.log(body);
+		  console.log(access_token);
         });
 
         // we can also pass the token to the browser to make requests from there
